731735bb139b9c205900a04d737ee36ed11d4418
Ivan <hubbu09@gmail.com>
2017-08-09 17:39:54 -0400

    Add camera relative player positioning. Orbit camera in 3rd person view.

---
+++ Ch07/Assets/Scripts/RelativeMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class RelativeMovement : MonoBehaviour {
+
+    [SerializeField] private Transform target;
+
+    public float moveSpeed = 6.0f;
+    public float rotSpeed = 15.0f;
+
+    private CharacterController charController;
+
+    void Start() {
+        charController = GetComponent<CharacterController>();
+    }
+
+    void Update () {
+        Vector3 movement = Vector3.zero;
+
+        float horInput = Input.GetAxis("Horizontal");
+        float vertInput = Input.GetAxis("Vertical");
+
+        if (horInput != 0 || vertInput != 0) {
+            movement.x = horInput * moveSpeed;
+            movement.z = vertInput * moveSpeed;
+            movement = Vector3.ClampMagnitude(movement, moveSpeed);
+
+            Quaternion tmpRot = target.rotation;
+            target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+            movement = target.TransformDirection(movement);
+            target.rotation = tmpRot;
+
+            /// Replaced with smooth rotation by using Lerp
+            /// Removing lerp and using only this line will cause snap rotations
+            // transform.rotation = Quaternion.LookRotation(movement);
+
+            Quaternion direction = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotSpeed * Time.deltaTime);
+        }
+
+        movement *= Time.deltaTime;
+        charController.Move(movement);
+    }
+}
