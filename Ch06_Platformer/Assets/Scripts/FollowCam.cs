cfb69b8f9a4c0afce8d1da286fb077a3e03cfc99
Ivan <ia@ivanarellano.com>
2017-08-14 11:38:24 -0400

    Finish Ch06_Platformer

---
+++ Ch06_Platformer/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+
+    [SerializeField] private Transform target;
+
+    private Vector3 vel = Vector3.zero;
+
+    public float smoothTime = 0.2f;
+
+    void LateUpdate() {
+        Vector3 targetPos = new Vector3(
+            target.position.x, target.position.y, transform.position.z);
+
+        transform.position = Vector3.SmoothDamp(transform.position,
+            targetPos, ref vel, smoothTime);
+    }
+}
