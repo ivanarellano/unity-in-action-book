3cc8a0b2972ec9f2513bc16a873fc21bbd002dd5
Ivan <hubbu09@gmail.com>
2017-08-15 20:41:54 -0400

    Start Ch10

---
+++ Ch10/Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour {
+
+    private int health;
+
+	void Start () {
+        health = 5;
+	}
+	
+	void Update () {
+		
+	}
+
+    public void Hurt(int damage) {
+        health -= damage;
+        Debug.Log("Health: " + health);
+    }
+}
---
+++ Ch10/Assets/Scripts/WanderingAI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingAI : MonoBehaviour {
+
+    public float speed = 3.0f;
+    public float obstacleRange = 5.0f;
+
+    [SerializeField] private GameObject fireballPrefab;
+    private GameObject fireball;
+    private bool alive;
+
+	void Start () {
+        alive = true;
+	}
+	
+	void Update () {
+        if (alive) {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if (Physics.SphereCast(ray, 0.75f, out hit)) {
+                GameObject target = hit.transform.gameObject;
+                if (target.GetComponent<PlayerCharacter>()) {
+                    if (fireball == null) {
+                        fireball = Instantiate(fireballPrefab) as GameObject;
+                        fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                        fireball.transform.rotation = transform.rotation;
+                    }
+                }
+                else if (hit.distance < obstacleRange) {
+                    float angle = Random.Range(-110, 110);
+                    transform.Rotate(0, angle, 0);
+                }
+            }
+        }
+	}
+
+    public void SetAlive(bool alive) {
+        this.alive = alive;
+    }
+}
